ab8807a43e91450bad002c5a84c50925a8bd2a62
Juliene Witória Viana <119975669+Jojobinea@users.noreply.github.com>
2025-02-13 08:16:06 -0400

    enemies and players on network and visuals

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -7,90 +7,49 @@ public class EnemyController : NetworkBehaviour
 {
     [SerializeField] private EnemyStatsSO _enemyStatsSO;
     private Transform _route;
-    private float _tParam;
-    private Vector3 _objectPosition;
+    private Transform _targetPos;
+    private Transform[] _waypoints;
     private float _speed;
-    private bool _coroutineAllowed;
-    private float _curveLength;
-    private const int samplePoints = 50;
-
+    private int _waypointIndex;
 
     void OnEnable()
     {
         _speed = _enemyStatsSO.speed;
         _route = GameObject.FindGameObjectWithTag("Route").transform;
 
-        _tParam = 0f;
-        _coroutineAllowed = true;
+        // Initialize the array
+        _waypoints = new Transform[_route.childCount];
+
+        for (int i = 0; i < _route.childCount; i++)
+        {
+            _waypoints[i] = _route.GetChild(i);
+        }
+
+        _waypointIndex = 0;
+        _targetPos = _waypoints[_waypointIndex];
     }
 
     void Update()
     {
-        if(!IsServer) return;
+        if (!IsServer) return;
             
-        if (_coroutineAllowed)
-        {
-            StartCoroutine(GoByTheRoute());
-        }
+        SetTargetPos();
+        transform.position = Vector3.MoveTowards(transform.position, _targetPos.position, _speed * Time.deltaTime);
     }
 
-    private IEnumerator GoByTheRoute()
+    private void SetTargetPos()
     {
-        _coroutineAllowed = false;
-
-        int pointsCount = _route.childCount;
-        Vector3[] controlPoints = new Vector3[pointsCount];
-
-        for (int i = 0; i < pointsCount; i++)
+        if (Vector3.Distance(transform.position, _targetPos.position) <= 0.1f) // Small threshold
         {
-            controlPoints[i] = _route.GetChild(i).position;
+            if (_waypointIndex < _waypoints.Length - 1)
+            {
+                _waypointIndex++;
+                _targetPos = _waypoints[_waypointIndex];
+            }
+            else
+            {
+                NetworkObject.Despawn();
+            }
         }
-
-        _curveLength = ApproximateCurveLength(controlPoints);
-        float travelDistance = 0f;
-        float step = _speed / _curveLength;
-
-        while (travelDistance < 1)
-        {
-            _tParam = travelDistance;
-            _objectPosition = CalculateBezierPoint(_tParam, controlPoints);
-            transform.position = _objectPosition;
-            travelDistance += step * Time.deltaTime;
-            yield return null;
-        }
-
-        _tParam = 0;
-        _coroutineAllowed = true;
-        NetworkObject.Despawn();
-    }
-
-    private Vector3 CalculateBezierPoint(float t, Vector3[] points)
-    {
-        if (points.Length == 1)
-            return points[0];
-
-        Vector3[] newPoints = new Vector3[points.Length - 1];
-        for (int i = 0; i < points.Length - 1; i++)
-        {
-            newPoints[i] = Vector3.Lerp(points[i], points[i + 1], t);
-        }
-
-        return CalculateBezierPoint(t, newPoints);
-    }
-
-    private float ApproximateCurveLength(Vector3[] points)
-    {
-        float length = 0f;
-        Vector3 prevPoint = points[0];
-
-        for (int i = 1; i <= samplePoints; i++)
-        {
-            float t = i / (float)samplePoints;
-            Vector3 currentPoint = CalculateBezierPoint(t, points);
-            length += Vector3.Distance(prevPoint, currentPoint);
-            prevPoint = currentPoint;
-        }
-
-        return length;
     }
 }
